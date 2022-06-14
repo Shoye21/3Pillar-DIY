4facfc744fe5eff49b9e0b58e494d1cf4272f82b
Shoye21 <shaurya.omar@gmail.com>
2022-06-14 13:36:40 +0530

    7.1

---
+++ Task 2/script.js
@@ -0,0 +1,17 @@
+var firstName = "Shaurya" , lastName = "Omar" , country = "India", age = 28, city = "Kanpur" , isMarried = true, year = 2022 ;
+console.log(typeof firstName , typeof lastName ,typeof isMarried ,typeof year ,typeof city,typeof age)
+
+if(typeof "10" == typeof 10){
+    console.log("true")
+}
+else{
+    console.log("False")
+}
+
+if(parseInt('9.8') == 10){
+    console.log("true")
+}
+else{
+    console.log("False")
+}
+
