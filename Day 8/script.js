f1f80dcdd2dec633570ca94d96a197781dc57869
Shoye21 <shaurya.omar@gmail.com>
2022-06-15 13:22:59 +0530

    9

---
+++ Day 8/script.js
@@ -0,0 +1,25 @@
+dog = {}
+console.log(typeof dog , dog)
+
+dog = {
+    name : "Shiro",
+    legs : 4,
+    color: "black",
+    age: 7,
+    bark: () => {
+        return "woof woof"
+    }
+}
+
+console.log(dog.age)
+console.log(dog.bark())
+console.log(dog.color)
+console.log(dog.name)
+console.log(dog.legs)
+
+dog.breed = "german shepherd";
+dog.getDogInfo = function () {
+    console.log(Object.values(dog));
+}
+dog.getDogInfo()
+
