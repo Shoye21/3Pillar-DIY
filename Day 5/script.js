44d7eeed4c450cdd903bf516536013090b40e06b
Shoye21 <shaurya.omar@gmail.com>
2022-06-15 11:58:13 +0530

    9

---
+++ Day 5/script.js
@@ -0,0 +1,16 @@
+let empty = []
+
+let work = ["eat", "sleep", "drink", "run", "walk"];
+
+console.log(work.length);
+
+let firstItem = console.log(work[0]);
+let middle = Math.floor((work.length) / 2);
+let middleItem = console.log(work[middle]);
+let lastItem = console.log(work[work.length - 1]);
+
+let mixed = ["sharya", 21, true, null, {
+    favoriteFood: ["pizza", "chicken"]
+}, undefined]
+
+let techCompanies = ["google", "microsoft", "oracle", "amazon", "ibm", "oracle", "apple"];
